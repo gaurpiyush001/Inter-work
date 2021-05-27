dc73be688b62d3e4962c912f3970a25367046d98
Piyush Gaur <gaurpiyush001@gmail.com>
2021-05-28 00:20:26 +0530

    demo-commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const slides = document.querySelectorAll(".slide");
+const dotContainer = document.querySelector(".dots");
+
+slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+
+// 0%, 100%, 200%, 300%
+
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+createDots();
+
+const activateDot = function (slide) {
+  document
+    .querySelectorAll(".dots__dot")
+    .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add("dots__dot--active");
+};
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+goToSlide(0);
+
+dotContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots__dot")) {
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+    activateDot(slide);
+  }
+});
